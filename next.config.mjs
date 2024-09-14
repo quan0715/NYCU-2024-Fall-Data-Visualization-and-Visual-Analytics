b7c9fa410c5637b5b37ee38e2aebcf48c6a52c57
quan0715 <quan787887@gmail.com>
2024-09-14 15:02:32 +0800

    update datasources

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+const isProd = process.env.NODE_ENV === "production";
+
+const nextConfig = {
+    basePath: isProd
+        ? "NYCU-2024-Fall-Data-Visualization-and-Visual-Analytics"
+        : "",
+    output: "export",
+};
 
 export default nextConfig;
